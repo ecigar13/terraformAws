5b310fab9f6bd2906b0ec1615777feb3062ebc74
Keith, Nguyen <keith.nguyen@sabre.com>
2020-06-08 21:39:38 -0500

    changes.

--- my-terraform-module/variables.secret.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
- description = "The AWS access key."
- default     = "XYXYXACCESSKEYXYXYX"
-}
-
-variable "aws_secret_key" {
- description = "The AWS secret key."
- default     = "XYXYSUPERSECRETKEYXYXYX"
-}
